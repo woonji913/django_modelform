8f150f867d242e3f10af424497971e523400ddf0
woonji913 <johnnyboy0913@gmail.com>
2019-04-08 08:56:00 +0000

    190408 | signup, login, logout구현

--- myform/settings.py
+++ myform/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = ['*']
 
 INSTALLED_APPS = [
     'boards.apps.BoardsConfig',
+    'accounts.apps.AccountsConfig',
     'crispy_forms',
     'django.contrib.admin',
     'django.contrib.auth',
