6089f7fc8ff2da061356e699e1e9e101a6bb3241
woonji913 <johnnyboy0913@gmail.com>
2019-04-09 06:38:22 +0000

    forms에서 username 변경부분 삭제

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,4 +4,4 @@ from django.contrib.auth.models import User
 class UserCustomChangeForm(UserChangeForm):
     class Meta:
         model = User
-        fields = ['username', 'email', 'first_name', 'last_name',]
+        fields = ['email', 'first_name', 'last_name',]
